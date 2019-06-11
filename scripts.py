117d27eb838dc800a3e6dfe9a1021c6d30b62402
Amir Akhter Kazi <kazi@uchicago.edu>
2019-06-10 21:11:48 -0500

    Eric overlooked that stuff

--- scripts.py
+++ scripts.py
@@ -11,6 +11,8 @@ df = pd.read_csv ('database.csv')
 
 
 # CHART 1: RECUSALS BY DATE; CLICK ON ALDERMAN
+output_file('recusals_by_date.html', mode='inline')
+
 recusals_df = df.groupby(( "Date", "Alderman")).size().to_frame().reset_index()
 recusals_df.rename(columns={0:"num_recusals"}, inplace=True)
 recusals_df = recusals_df.pivot("Date", "Alderman", "num_recusals")
@@ -24,12 +26,15 @@ chart1 = recusals_df.plot_bokeh.bar(
     alpha = 1)
 
 
-output_file('recusals_by_date.html', mode='inline')
+
 save(chart1)
 
 
 
 # CHART 2: ALDERMAN BY TYPE OF RECUSAL
+
+output_file('recusals_by_reason.html', mode='inline')
+
 recusals_df = df.groupby(( "Alderman", "Recusal_Reason")).size().to_frame().reset_index()
 recusals_df.rename(columns={0:"num_recusals"}, inplace=True)
 recusals_df = recusals_df.pivot( "Alderman", "Recusal_Reason", "num_recusals")
@@ -43,11 +48,13 @@ chart2 = recusals_df.plot_bokeh.bar(
     alpha = 1)
 
 
-output_file('recusals_by_reason.html', mode='inline')
+
 save(chart2)
 
 
 # CHART 3: RECUSALS BY SUBJECT; CLICK ON Subject
+
+output_file('recusals_by_subject.html', mode='inline')
 
 recusals_df = df.groupby(("Alderman", "Subject")).size().to_frame().reset_index()
 recusals_df.rename(columns={0:"num_recusals"}, inplace=True)
@@ -61,13 +68,13 @@ chart3 = recusals_df.plot_bokeh.bar(
     title = "Recusals by Subject",
     alpha = 1)
 
-output_file('recusals_by_subject.html', mode='inline')
+
 save(chart3)
 
 
 # CHART 4: DATA TABLE
 
-
+output_file('data_table.html', mode='inline')
 source = ColumnDataSource(df)
 
 columns = [
@@ -81,5 +88,5 @@ columns = [
 data_table = DataTable(source=source, columns=columns, width=1400, height=1000)
 
 
-output_file('data_table.html', mode='inline')
+
 save(data_table)
